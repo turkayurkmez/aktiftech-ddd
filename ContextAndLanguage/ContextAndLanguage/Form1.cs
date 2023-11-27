1dc737f9a6ab1fa5fb0c6bc74b7b2814ef4aec4f
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-11-27 13:58:27 +0300

    Start new project

---
+++ ContextAndLanguage/ContextAndLanguage/Form1.cs
@@ -0,0 +1,10 @@
+namespace ContextAndLanguage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ContextAndLanguage/ContextAndLanguage/SickLeaveForApproval.cs
@@ -0,0 +1,20 @@
+﻿namespace ContextAndLanguage
+{
+    public class SickLeaveForApproval
+    {
+        public int EmployeeId { get; set; }
+        public DateTime DateRegistered { get; set; }
+        public DateTime FirstDayNotAWork { get; set; }
+        public bool LeftDuringWorkDay { get; set; }
+        public DateTime ComeBackToWork { get; set; }
+        public bool ComeBackAfterLunch { get; set; }
+    }
+
+    public class SickLeaveApplication
+    {
+        public void Handle(SickLeaveForApproval sickLeaveForApproval)
+        {
+            //db'ye kaydet!
+        }
+    }
+}
